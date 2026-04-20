5ad8563188d5332cd70a4dd45f8d4f4a761c5401
chanbinchoi <classy9611@gmail.com>
2026-04-20 22:55:04 +0900

    Create Product.java, Application.java

--- src/main/java/com/nagoya/tracker/domain/Product.java
+++ src/main/java/com/nagoya/tracker/domain/Product.java
@@ -1,4 +1,13 @@
 package com.nagoya.tracker.domain;
 
 public class Product {
+    public String storeName;
+    public String itemName;
+    public int price;
+
+    public Product(String storeName, String itemName, int price) {
+        this.storeName = storeName;
+        this.itemName = itemName;
+        this.price = price;
+    }
 }
---
+++ src/main/java/com/nagoya/tracker/main/Application.java
@@ -0,0 +1,10 @@
+package com.nagoya.tracker.main;
+
+import com.nagoya.tracker.domain.Product;
+
+public class Application {
+    public static void main(String[] args) {
+        Product vdrug_banana = new Product("vdrug", "banana", 150);
+        Product yamanaka_banana = new Product("yamanaka", "banana", 180);
+    }
+}

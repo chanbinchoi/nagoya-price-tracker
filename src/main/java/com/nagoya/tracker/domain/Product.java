b0868d8046b57a384cb76fc839118f727f346a47
Chanbin Choi <classy9611@gmail.com>
2026-05-07 17:35:03 +0900

    ~ read csv

--- src/main/java/com/nagoya/tracker/domain/Product.java
+++ src/main/java/com/nagoya/tracker/domain/Product.java
@@ -1,9 +1,21 @@
 package com.nagoya.tracker.domain;
 
 public class Product {
-    public String storeName;
-    public String itemName;
-    public int price;
+    final private String storeName;
+    final private String itemName;
+    final private int price;
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 
     public Product(String storeName, String itemName, int price) {
         this.storeName = storeName;
--- src/main/java/com/nagoya/tracker/main/Application.java
+++ src/main/java/com/nagoya/tracker/main/Application.java
@@ -2,9 +2,48 @@ package com.nagoya.tracker.main;
 
 import com.nagoya.tracker.domain.Product;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        Product vdrug_banana = new Product("vdrug", "banana", 150);
-        Product yamanaka_banana = new Product("yamanaka", "banana", 180);
+        // 1. Create productList
+        List<Product> productList = new ArrayList<>();
+
+        // 2. Read File
+        try (BufferedReader br = new BufferedReader(new FileReader("prices.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+
+                // 3. add List
+                if (values.length == 3) {
+                    productList.add(new Product(values[0], values[1], Integer.parseInt(values[2])));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // 4. Create productMap
+        Map<String, Product> productMap = new HashMap<>();
+
+        // 5. put productMap
+        for (Product p : productList) {
+            String compositeKey = p.getStoreName() + "_" + p.getItemName();
+            productMap.put(compositeKey, p);
+        }
+
+        // 6. Search for Optional
+        Optional<Product> resultOpt = Optional.ofNullable(productMap.get("Amica_banana"));
+
+        // 7. Print
+        resultOpt.ifPresentOrElse(
+                p -> System.out.println("store: " + p.getStoreName() + ", price: " + p.getPrice()),
+                () -> System.out.println("Data is nothing")
+        );
+
     }
 }
